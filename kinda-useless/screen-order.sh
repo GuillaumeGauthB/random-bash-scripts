9ac443160080c1f2b207f77cdfda2377e8351fb3
GuillaumeGauthB <guillaume.gb@live.ca>
2022-02-05 16:42:00 -0500

    Added wallpaper-qtile.sh & screen-order.sh

---
+++ kinda-useless/screen-order.sh
@@ -0,0 +1,1 @@
+xrandr --output HDMI-0 --primary --output DP-5 --right-of HDMI-0
---
+++ useful/wallpaper-qtile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+tree -laL 2 ~/Pictures
+echo
+read -p "Select path to picture: " Path;
+echo
+ls ~/Pictures/$Path
+echo
+read -p "Which picture do you wanna select: " PicSelection;
+if [[ "${PicSelection,,}" == "$(ls ~/Pictures/$Path | grep $PicSelection)" ]]; then
+    xrandr --listmonitors
+    echo
+    read -p "Choose which monitor to change: " Monitor;
+    hardlink="currentImage$Monitor"
+    rm -rfv ~/Pictures/currentwp/$hardlink
+    ln ~/Pictures/$Path/$PicSelection ~/Pictures/currentwp/$hardlink
+else
+    echo learn to write correctly
+fi
