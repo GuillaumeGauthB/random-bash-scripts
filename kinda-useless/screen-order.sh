3c7e18ee67fd9bb9f57214451883831944139998
Guillaume Gauthier-Benoit <guillaume.gb@live.ca>
2022-05-22 19:34:23 -0400

    Made create-sinks-streaming.sh automatic, and changed screen-order a bit

--- kinda-useless/screen-order.sh
+++ kinda-useless/screen-order.sh
@@ -1,1 +1,4 @@
-xrandr --output HDMI-0 --primary --output DP-5 --right-of HDMI-0
+xrandr --output HDMI-0 --primary --output DP-5 --right-of HDMI-0 --output DVI-D-0 --left-of HDMI-0
+# xrandr --output HDMI-0 --auto --primary --output DVI-D-0 --auto --left-of HDMI-0 --output DP-5 --auto --right-of HDMI-0
+# xrandr --output DVI-D-0 --primary --left-of HDMI-0
+# xrandr --output HDMI-0 --primary --output DP-5 --right-of HDMI-0
--- useful/create-sinks-streaming.sh
+++ useful/create-sinks-streaming.sh
@@ -1,31 +1,67 @@
 #!/bin/bash
+#
 #		Script that creates two audio sinks: one that is null and another that is a combination of your chosen sink and the null one
 #
-#		Makes streaming easier, stream the combination, put the null sink on sounds you want the viewers to hear but not you and put
-#		your original sink the sounds you want only you to hear
-#
+#		Useful when streaming. 
+#   
+#   Null sink is used for sounds you want the audience to hear but not you
+#   Original sink is used for sounds you want to hear but not the audience
+#   Mix is used for sounds you want everyone to hear
+# 
+#   Some changes need to be made to your OBS sound sinks for this to work
 
+# Function to make things more readable for the user
+reading () {
+  echo
+  echo "__________________________________"
+  echo
+}
 
-# check if pulse is on the computer
-location="$(ls /usr/bin | grep pulseaudio)"
-if [[ "${location,,}" == *"pulseaudio"* ]]; then
-    ls
-    pactl list sinks short
-    echo ____________________________________________________________________________________
-    echo;
-    read -p "Input the name of the audio sink you are using: " sinkInUse
-    if [[ "${sinkInUse,,}" == "$(pactl list sinks short | grep ${sinkInUse,,})" ]]; then
-      pactl load-module module-null-sink sink_name=Null
-      pactl load-module module-combine-sink sink_name=Combined slaves=Null,$sinkInUse
-    else
-      echo "Didn't write it correctly ya dummy"
-    fi
+# Function to manually choose which sink to use
+manualChoice () {
+  echo "Listing sinks :"
+  reading
+  pactl list sinks short
+  reading
+  read -p "Input the name of the audio sink you are using: " sinkInUse
+  if [[ "${sinkInUse,,}" == "$(pactl list sinks short | grep ${sinkInUse,,})" ]]; then
+    pactl load-module module-null-sink sink_name=Null
+    pactl load-module module-combine-sink sink_name=Combined slaves=Null,$sinkInUse
+  else
+    echo "Didn't write it correctly ya dummy"
+  fi
+}
+
+# Ask user whether or not they want the merge to be automatic
+read -p "Make sink automatically? (Y/n)": theChoice
+
+# if the user answers with y or answers with nothing, then do it automatically
+if [[ "$theChoice" == "" || "${theChoice,,}" == "y" ]]; then
+  
+  # Print all sinks to allSinks, then sort allSinks to only get the sinks that are running
+  allSinks="$(pactl list sinks short)"
+  running=$(awk -F " " '/RUNNING/ {print $2}' <<< "$allSinks")
+
+  # If there is only one running sink, make the merge automatically
+  if [[  $(echo "$running" | wc -l) == 1  && $(echo "$running") != ""  ]]; then
+  
+    pactl load-module module-null-sink sink_name=Null
+    pactl load-module module-combine-sink sink_name=Combined slaves=Null,$running
+  
+  else
+    # otherwise, make the user choose manually
+    echo "Cannot automatically make the sink"
+    reading
+    manualChoice
+  fi
+
+elif [[ "${theChoice,,}" == "n" ]]; then
+  # if the user says no, make the user choose manually
+  echo "Making manual merge"
+  reading
+  manualChoice
 else
-    echo ____________________________________________________________________________________
-    echo
-    echo You do not have pulseaudio, the script will not work
-    echo
-    echo Download it wichever way works best with your distro
-    echo
-    echo ____________________________________________________________________________________
+  # otherwise, error
+  echo "Answer does not make sense"
+  echo "Exitting..."
 fi
