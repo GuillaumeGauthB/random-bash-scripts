682ddc3ac3bac3d10d692225e9e9e93979ebc0b5
Guillaume Gauthier-Benoit <guillaume.gb@live.ca>
2022-05-26 19:17:44 -0400

    Fix if user does not have tree?

--- useful/git-push.sh
+++ useful/git-push.sh
@@ -1,34 +1,50 @@
 #!/bin/bash
 # Script to automatically push
 
+# Command not found
+
 # Function to add files to commit
 addFile () {
-    tree $(git rev-parse --show-toplevel)
-        echo ""
-        echo "__________________"
-        echo ""
-        echo "What do you want to add?"
-        echo "PS: for now only works with a single added element"
-        read toAdd
-        if [[ "$toAdd" != "" || "$(tree $(git rev-parse --show-toplevel) | grep $toAdd )" != "" ]]; then
-            IFS=' ' read -r -a toAddArray <<< "$toAdd"
-            for item in "${toAddArray[@]}"; do
-                echo "$item"
-                if [[ "$(find ~+ -name $item)" != "" ]]; then
-                    echo $(find ~+ -name $item)
-                    git add $(find ~+ -name "$item")
-                fi
-            done
-        elif [[ "$toAdd" == "." ]]; then
-            git add .
-        else
-            echo "File or folder does not exist, try again"
-            addFile
-            return 1
-        fi
-        read -p "Commit name: " commitName
-        git commit -m "$commitName"
-        pushFile
+    # tree $(git rev-parse --show-toplevel)
+    echo "Modified files:"
+    echo ""
+    git ls-files -m
+    echo "__________________"
+    echo "Untracked Files:"
+    echo ""
+    git ls-files --others --exclude-standard
+    echo "__________________"
+    echo ""
+    echo "__________________"
+    echo ""
+    echo "What do you want to add?"
+    echo "PS: for now only works with a single added element"
+    read toAdd
+    if [[ "$(tree)" == *"not found"* ]]; then
+        echo "Tree command not found"
+        # list all directories in folder
+        # for every directory, execute a command that checks for other directories and executes a function on all of them, that will
+        # reexecute the function 
+        # 
+    elif [[ "$toAdd" != "" || "$(git ls-files -m | grep $toAdd )" != "" || "$()git ls-files --others --exclude-standard | grep $toAdd" != "" ]]; then
+        IFS=' ' read -r -a toAddArray <<< "$toAdd"
+        for item in "${toAddArray[@]}"; do
+            echo "$item"
+            if [[ "$(find ~+ -name $item)" != "" ]]; then
+                echo $(find ~+ -name $item)
+                git add $(find ~+ -name "$item")
+            fi
+        done
+    elif [[ "$toAdd" == "." ]]; then
+        git add .
+    else
+        echo "File or folder does not exist, try again"
+        addFile
+        return 1
+    fi
+    read -p "Commit name: " commitName
+    git commit -m "$commitName"
+    pushFile
 }
 
 # Function to set where to push
@@ -67,7 +83,6 @@ if [[ $1 == "" || $2 == "" || $3 == "" ]]; then
     read -p "User CLI interface? (Y/n)" theChoice
     if [[ "${theChoice}" == "" || "${theChoice,,}" == "y" ]]; then
         addFile
-        
     else
         echo "Exitting"
     fi
